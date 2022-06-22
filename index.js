c459d2ee4e7613c19745e3a2a400daf62642dee2
aalonso0132 <aalonsopersonal@gmail.com>
2022-06-22 03:15:29 +0200

    Project set up. Index file created and first version of the server working.

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const http = require('http');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+mongoose.set('debug', true);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const uri = 'mongodb://localhost:27017/events';
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex: true,
+};
+
+mongoose.connect(uri, options);
+mongoose.connection.once('open', function () {
+    console.log("MongoDB database connection established successfully");
+});
+
+app.get('/', function (req, res) {
+    res.status(200).send(
+        {
+            "msg": "hello_world"
+        }
+    )
+})
+
+async function setup_and_launch_server(app) {
+
+    // catch 404 and forward to error handler
+    app.use((req, res) => {
+        res.status(404).send();
+    });
+
+    server.listen(process.env.PORT || 3000, function () {
+        console.log('Listening');
+    });
+}
+
+setup_and_launch_server(app).then(r => console.log('Server launched'));
